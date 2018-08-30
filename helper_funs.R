fed9a6b0ec246472929a96bd9ecd40f20a893985
Nathan T. James <nj1154@gmail.com>
2018-08-30 14:09:31 -0500

    add helper functions
    detach all packages

---
+++ helper_funs.R
@@ -0,0 +1,13 @@
+
+# from https://stackoverflow.com/questions/7505547/detach-all-packages-while-working-in-r
+detachAllPackages <- function() {
+  
+  basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+  
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  
+  package.list <- setdiff(package.list,basic.packages)
+  
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+  
+}
